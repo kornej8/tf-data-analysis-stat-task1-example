c95bb99e58d2f19db7f5210954f01551ca54ef02
Kornej Shchepalov <115864132+kornej8@users.noreply.github.com>
2023-04-06 03:03:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ import numpy as np
 chat_id = 720721680 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a = np.mean(x) / 10
+    a = 10 / np.mean(x)
     return a
 # a = 10 / np.mean(x)
